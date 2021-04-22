5637ba3e8db605f2d13c6994f85024ec956cd5f7
Akim <dektyannikov.akim@mail.ru>
2021-04-22 21:11:04 +0700

    добавил стили и аргументы для функции

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const goods = [
+    { title: 'Shirt', price: 150 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+];
+
+const renderGoodsItem = (title = "товар отсутствует", price = "цена еще не известна") => {
+    return `<figure class="goods-item"><img src="#" alt="#"><figcaption class="item-info"><h3>${title}</h3><span>${price}</span></figcaption></figure>`;
+};
+
+const renderGoodsList = (list) => {
+    let goodsList = document.querySelector('.goods-list');
+    goodsList.innerHTML = list.map(item => renderGoodsItem(item.title, item.price)).join('');
+}
+
+renderGoodsList(goods);
