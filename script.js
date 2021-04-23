372a9ef56c9e1187862650f62d8775d1da27509e
Akim <dektyannikov.akim@mail.ru>
2021-04-23 16:07:49 +0700

    Добавил значение по умолчанию для img

--- script.js
+++ script.js
@@ -6,17 +6,19 @@ const products = [
     { title: 'Shirt', price: 150, img: `https://picsum.photos/seed/${getRandomNumber()}/260/280` },
     { title: 'Socks', price: 50, img: `https://picsum.photos/seed/${getRandomNumber()}/260/280` },
     { title: 'Jacket', price: 350, img: `https://picsum.photos/seed/${getRandomNumber()}/260/280` },
-    { title: 'Shoes', price: 250, img: `https://picsum.photos/seed/${getRandomNumber()}/260/280` },
+    { title: 'Shoes', price: 250 },
 ];
 
-const renderProductItem = (product) =>
-    `<figure class="products-item">
+const renderProductItem = (product) => {
+    product.img = typeof product.img !== 'undefined' ? product.img : "img/product-1.png";
+    return `<figure class="products-item">
     <img src="${product.img}" alt="Фото товара">
     <figcaption class="item-info">
     <h3>${product.title}</h3>
     <span class="red">${product.price}$</span>
     <button class="product-buy" type="button">Купить</button>
-    </figcaption></figure>`;
+    </figcaption></figure>`
+};
 
 const renderProductList = (list) => {
     let productsList = document.querySelector('.products-list');
