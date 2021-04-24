f53a796fc05b2d14265ba34e4b3e1a270a10a981
Ivan Bedarev <iabedarev@gmail.com>
2021-04-24 22:08:38 +0300

    FastFood improvement

--- fastfood/script.js
+++ fastfood/script.js
@@ -1,80 +1,50 @@
 'use strict'
 
-const menu = [{
-        position: "bigMac",
-        calories: 40,
-        price: 100,
-    },
-    {
-        position: "smallMac",
-        calories: 20,
-        price: 50,
-    },
-    {
-        position: "cheese",
-        calories: 20,
-        price: 10,
-    },
-    {
-        position: "salad",
-        calories: 5,
-        price: 20,
-    },
-    {
-        position: "potatoes",
-        calories: 10,
-        price: 15,
-    },
-    {
-        position: "seasoning",
-        calories: 0,
-        price: 15,
-    },
-    {
-        position: "mayonnaise",
-        calories: 5,
-        price: 20,
-    },
-];
+const menu = {
+    bigMac: { calories: 40, price: 100 },
+    cheese: { calories: 20, price: 10 },
+    mayonnaise: { calories: 5, price: 20 },
+    potatoes: { calories: 10, price: 15 },
+    salad: { calories: 5, price: 20 },
+    seasoning: { calories: 0, price: 15 },
+    smallMac: { calories: 20, price: 50 }
+}
 
-
-class Hamburger {
-    constructor(size, stuffing) {
-        this.size = size;
-        this.stuffing = [stuffing];
+class Order {
+    constructor(items) {
+        this.items = items;
     }
 
-    addSauce(sauce) {
-        this.stuffing.push(sauce);
+    addItem(item) {
+        this.items.push(item)
     }
 
-    removeSauce(sauce) {
-        this.stuffing.splice(this.stuffing.indexOf(sauce), 1);
+    removeItem(item) {
+        this.items.splice(this.items.indexOf(item), 1);
     }
 
     calculatePrice() {
         let totalPrice = 0;
-        totalPrice += menu.find(positionItem => positionItem.position === this.size).price;
-        this.stuffing.forEach(position => {
-            totalPrice += menu.find(positionItem => positionItem.position === position).price;
-        });
-
+        this.items.forEach(item => {
+            totalPrice += menu[item].price
+        })
         return totalPrice;
+        //или одной строкой через reduce (не очень люблю эту функцию, не очень легко читается)
+        //this.items.reduce((price, item) => price + menu[item].price, 0)
     }
 
     calculateCalories() {
         let totalCalorie = 0;
-        totalCalorie += menu.find(positionItem => positionItem.position === this.size).calories;
-        this.stuffing.forEach(position => {
-            totalCalorie += menu.find(positionItem => positionItem.position === position).calories;
-        });
+        this.items.forEach(item => {
+            totalCalorie += menu[item].calories
+        })
 
         return totalCalorie;
     }
 }
 
-const bigMack = new Hamburger('smallMac', 'cheese');
-bigMack.addSauce('mayonnaise');
-bigMack.removeSauce('mayonnaise');
+const bigMack = new Order(['smallMac', 'cheese']);
+bigMack.addItem('mayonnaise');
+bigMack.removeItem('mayonnaise');
 console.log(bigMack.calculatePrice());
 console.log(bigMack.calculateCalories());
